25d9ab22636d1af957c048a97c2d754944816bf7
TmaPrisla <mikhvilvolobuev@gmail.com>
2021-09-12 23:21:27 +0500

    Любой комментарий

--- firstPython.py
+++ firstPython.py
@@ -1,1 +1,17 @@
-print("Хуй")
+import time
+
+import requests
+while True:
+    i = requests.get("http://api.exchangeratesapi.io/v1/latest?access_key=df0fa438a8cdf3661bcd021419339dd4&symbols=USD,AUD,CAD,PLN,MXN")
+
+    print()
+    import json
+    a=json.loads(i.text)
+    print()
+    del a["success"]
+    del a["timestamp"]
+    with (open("htyui.json","w")) as file:
+        json.dump(a, file)
+    file.close()
+    time.sleep(3)
+    print(a)
